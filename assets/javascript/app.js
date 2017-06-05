18289b27dbddcccbe9798bd62d9a840693bcf211
Andy Medina <Andy Medina>
2017-06-04 23:23:35 -0600

    yes

---
+++ assets/javascript/app.js
@@ -0,0 +1,37 @@
+$(document).ready()
+//question1
+"What indicates a basic pH on a universal scale usually?"
+{
+
+// answer1
+var answer1 = "A blueish color"
+// answer2
+var answer2 = "A reddish color"
+// answer3
+var answer3 = "A clear color"
+
+answer1 === True
+
+},
+
+// question2 
+""
+{
+
+// answer1
+// answer2
+// answer3
+},
+
+// question3 {
+
+// answer1
+// answer2
+// answer3
+}
+
+answer1.append()
+
+$(".").on("click", function() {
+	
+}
